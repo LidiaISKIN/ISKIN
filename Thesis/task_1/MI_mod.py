19bf912c867fd93448ce904721e9dc8bbbf229f2
LidiaISKIN <iskinlidia@gmail.com>
2020-09-25 20:27:31 +0300

    add mod_1

---
+++ Thesis/task_1/MI_mod.py
@@ -0,0 +1,10 @@
+IO_set_1 = set("hello","drug",('media','file'))
+IO_set_2 = set('hello','secret',('media','file'))
+IO_set_ij = set('hello',frozenset({'data'}))
+def Set_AI (*kwargs):
+    for token in kwargs:
+        if token = frozenset({"data from expert"}):
+            result = "Alarm"
+            else:
+            continue
+    return result
---
+++ Thesis/task_1/gf.py
@@ -0,0 +1,15 @@
+
+IO_set_1 = set("hello","drug",('media','file'))
+IO_set_2 = set('hello','secret',('media','file'))
+IO_set_ij = set('hello',frozenset({'data'}))
+def MI_mod(*args):
+    result = 'Alarm'
+    token = 0
+    for token in IO_set_1:
+        if token = len(drug, secret or frozenset({"data"}):
+            result = "Alarm"
+        else:
+            continue
+    return result
+    print(f'{mod_MI(IO_set_1=input("Введите множество))}')
+
